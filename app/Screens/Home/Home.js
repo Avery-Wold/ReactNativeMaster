f2083a2198589a644e3f2aa037eef0e325f31369
Avery Wold <AveryW@magenic.com>
2018-02-12 14:30:19 -0600

    code cleanup

--- app/Screens/Home/Home.js
+++ app/Screens/Home/Home.js
@@ -6,12 +6,23 @@ import {
 import styles from './styles'
 
 export default class Home extends Component {
+    static navigationOptions = {
+        title: 'Home',
+        headerStyle: {
+          backgroundColor: '#4B0082',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      };
+
     render() {
         const { params } = this.props.navigation.state;
         const name = params ? params.name : null;
         return (
             <View style={styles.container}>
-                <Text style={styles.welcome}>{JSON.stringify('Welcome, '+ name + '!')}</Text>
+                <Text style={styles.welcome}>{'Welcome, '+ name + '!'}</Text>
             </View>
         );
     }
